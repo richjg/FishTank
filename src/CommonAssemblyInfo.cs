9ad7e3c0489d8f0f26110c35ab11d5153222b302
Richard Godfrey <richjgodfrey@hotmail.com>
2017-06-22 11:23:59 +0100

    added assembly info

---
+++ src/CommonAssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
