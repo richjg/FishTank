d17467db98344560d2c2a2f8d53f4c857a92aabc
Richard Godfrey <richjgodfrey@hotmail.com>
2017-06-22 11:30:21 +0100

    starting to add the code

---
+++ src/FishTank/FishTank/IFish.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FishTank
+{
+    public interface IFish
+    {
+        double FoodRequired { get; }
+        string Name { get; set; }
+    }
+}
