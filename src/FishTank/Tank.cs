a687b0b680d3d08df847478f2621d8d348bd17d7
Richard Godfrey <richjgodfrey@hotmail.com>
2017-06-22 12:53:29 +0100

    Implementation of tank

--- src/FishTank/Tank.cs
+++ src/FishTank/Tank.cs
@@ -8,5 +8,17 @@ namespace FishTank
 {
     public class Tank
     {
+        private List<IFish> fish = new List<IFish>();
+
+        public Tank Add(IFish fish)
+        {
+            this.fish.Add(fish);
+            return this;
+        }
+
+        public int Count => this.fish.Count;
+        public IReadOnlyList<IFish> Fish => this.fish;
+
+        public double Feed() => Math.Round(this.fish.Sum(f => f.FoodRequired), 3);
     }
 }
--- tests/FishTank.Tests/TankTests.cs
+++ tests/FishTank.Tests/TankTests.cs
@@ -11,10 +11,81 @@ namespace FishTank.Tests
     public class TankTests
     {
         [Test]
-        public void TestMethod()
+        public void Add_Adds_FishToTheTank()
         {
-            // TODO: Add your test code here
-            Assert.Pass("Your first passing test");
+            //Arrange
+            var tank = new Tank();
+            var fish = new GoldFish { Name = "gold" };
+
+            //Act
+            tank.Add(fish);
+
+            //Assert
+            Assert.That(tank.Count, Is.EqualTo(1));
+            Assert.That(tank.Fish.First(), Is.SameAs(fish));
+        }
+
+        [Test]
+        public void Add_Adds_ManyFishToTheTank()
+        {
+            //Arrange
+            var tank = new Tank();
+
+            //Act
+            tank.Add(new GoldFish { Name = "gold1" })
+                .Add(new BabelFish { Name = "babel1" })
+                .Add(new AngelFish { Name = "angel1" })
+                .Add(new GoldFish { Name = "gold2" })
+                .Add(new BabelFish { Name = "babel2" })
+                .Add(new AngelFish { Name = "angel2" });
+
+            //Assert
+            Assert.That(tank.Count, Is.EqualTo(6));
+
+            Assert.That(tank.Fish.ElementAt(0), Is.InstanceOf<GoldFish>());
+            Assert.That(tank.Fish.ElementAt(0).Name, Is.EqualTo("gold1"));
+            Assert.That(tank.Fish.ElementAt(1), Is.InstanceOf<BabelFish>());
+            Assert.That(tank.Fish.ElementAt(1).Name, Is.EqualTo("babel1"));
+            Assert.That(tank.Fish.ElementAt(2), Is.InstanceOf<AngelFish>());
+            Assert.That(tank.Fish.ElementAt(2).Name, Is.EqualTo("angel1"));
+
+            Assert.That(tank.Fish.ElementAt(3), Is.InstanceOf<GoldFish>());
+            Assert.That(tank.Fish.ElementAt(3).Name, Is.EqualTo("gold2"));
+            Assert.That(tank.Fish.ElementAt(4), Is.InstanceOf<BabelFish>());
+            Assert.That(tank.Fish.ElementAt(4).Name, Is.EqualTo("babel2"));
+            Assert.That(tank.Fish.ElementAt(5), Is.InstanceOf<AngelFish>());
+            Assert.That(tank.Fish.ElementAt(5).Name, Is.EqualTo("angel2"));
+        }
+
+        [Test]
+        public void Feed_Returns_ZeroWhenNoFishInTank()
+        {
+            //Arrange
+            var tank = new Tank();
+            
+            //Act
+            var amount = tank.Feed();
+
+            //Assert
+            Assert.That(amount, Is.Zero);
+        }
+        
+        [Test]
+        public void Feed_Returns_TheSumOfTheFishFoodRequired()
+        {
+            //Arrange
+            var tank = new Tank();
+
+            //Act
+            tank.Add(new GoldFish { Name = "gold1" })
+                .Add(new BabelFish { Name = "babel1" })
+                .Add(new AngelFish { Name = "angel1" });
+
+            //Act
+            var amount = tank.Feed();
+
+            //Assert
+            Assert.That(amount, Is.EqualTo(0.6));
         }
     }
 }
